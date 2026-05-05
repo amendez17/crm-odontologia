1e1091cffe47a5184469c89164e173f10d9f12e7
amendez17 <74200859+amendez17@users.noreply.github.com>
2026-05-05 14:28:49 -0700

    Update PacienteDetalle.jsx
    Odontograma 2

--- client/src/pages/PacienteDetalle.jsx
+++ client/src/pages/PacienteDetalle.jsx
@@ -63,7 +63,7 @@ export default function PacienteDetalle() {
       toast.error('Error al actualizar odontograma');
     }
   };*/
-  const handleOdontograma = async (pieza, estado) => {
+ /* const handleOdontograma = async (pieza, estado) => {
 
   // 🔥 UI INMEDIATA (esto arregla tu problema visual)
   setOdontograma(prev => {
@@ -88,6 +88,15 @@ export default function PacienteDetalle() {
   } catch (error) {
     console.error('Error guardando odontograma:', error);
   }
+};*/
+  const handleOdontograma = async (pieza, estado, cara = null) => {
+  await axios.post('/api/odontograma', {
+    pieza_dental: pieza,
+    estado,
+    cara
+  });
+
+  obtenerOdontograma(); // refresca
 };
 
   const guardarHistoria = async (e) => {
