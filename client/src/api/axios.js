cdfd016eb5cb59e67c6b6cdc8368854b81424338
amendez17 <74200859+amendez17@users.noreply.github.com>
2026-05-04 20:59:21 -0700

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
-  headers: { 'Content-Type': 'application/json' }
+  baseURL: import.meta.env.VITE_API_URL, // 👈 SOLO la URL base del backend
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
