ecb889bd0b5f5c30175cf67c69f46f234173f67a
amendez17 <74200859+amendez17@users.noreply.github.com>
2026-05-05 07:38:07 -0700

    Update PacienteDetalle.jsx

--- client/src/pages/PacienteDetalle.jsx
+++ client/src/pages/PacienteDetalle.jsx
@@ -240,8 +240,8 @@ const imprimirConsentimiento = (c, paciente = {}) => {
   </div>
 
   <div class="info">
-    <strong>Paciente:</strong> ${paciente.nombre|| ''} ${paciente.apellido|| ''} <br> 
-    <strong>DNI:</strong> ${paciente.dni || ''}<br>
+    <strong>Paciente:</strong> ${paciente.nombre|| ''} ${paciente.apellido|| ''} <br>
+    <strong>DNI:</strong> ${paciente.dni|| ''}<br> 
     <strong>Doctor:</strong> Dr. ${c.doctor?.nombre || ''} ${c.doctor?.apellido || ''}<br>
     <strong>Fecha:</strong> ${c.createdAt ? c.createdAt.split('T')[0] : ''}
   </div>
