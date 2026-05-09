0b18e8f0f27f9bed51d092a698007a62c10cb726
amendez17 <74200859+amendez17@users.noreply.github.com>
2026-05-09 14:28:42 -0700

    Update error message for recipe permissions

--- client/src/pages/PacienteDetalle.jsx
+++ client/src/pages/PacienteDetalle.jsx
@@ -1024,7 +1024,7 @@ window.onload = () => window.print();
 
   } catch (error) {
     console.error(error);
-    toast.error('No tienes permisos para eliminar esta receta');
+    toast.error('No tienes permisos para actualizar esta receta');
   }
 };
   return (
