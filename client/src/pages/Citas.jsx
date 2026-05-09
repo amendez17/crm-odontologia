b3204b12a0bc8551c86d1947b8ae2b692224c7b6
amendez17 <74200859+amendez17@users.noreply.github.com>
2026-05-08 19:59:41 -0700

    Update Citas.jsx

--- client/src/pages/Citas.jsx
+++ client/src/pages/Citas.jsx
@@ -1,10 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import api from '../api/axios';
 import Modal from '../components/Modal';
 import toast from 'react-hot-toast';
 import { FiFileText } from 'react-icons/fi';
 import {FiPlus, FiEdit2, FiTrash2, FiChevronLeft, FiChevronRight,FiList, FiGrid, FiFilter, FiMessageCircle, FiCalendar} from 'react-icons/fi';
-import { useState, useEffect, useMemo } from 'react';
 
 
 const ESTADOS = {
