90ee82f695e23e6ca5e855b2a364009058f96314
amendez17 <74200859+amendez17@users.noreply.github.com>
2026-05-12 15:34:19 -0700

    Change file naming convention for presupuesto

--- client/src/pages/Presupuestos.jsx
+++ client/src/pages/Presupuestos.jsx
@@ -273,7 +273,7 @@ const numeroPaciente =
   p.paciente?.dni || p.paciente?.id || 'SIN_ID';
 
 const nombreArchivo =
-  `PAC-${numeroPaciente}_${nombrePaciente}_PRESUPUESTO_${p.id}`;
+  `CA-${numeroPaciente}_${nombrePaciente}_PRESUPUESTO_${p.id}`;
   win.document.write(`
 <!DOCTYPE html>
 <html>
