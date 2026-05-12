58a81d995e5a82a6ad8d7d776ef18bedb6b46115
amendez17 <74200859+amendez17@users.noreply.github.com>
2026-05-11 22:20:32 -0700

    Fix conditional rendering for current date

--- client/src/pages/Citas.jsx
+++ client/src/pages/Citas.jsx
@@ -717,7 +717,7 @@ const renderVistaSemana = () => {
               ))}
               {/* LINEA HORA ACTUAL */}
 
-{dia === getFechaLocal() && (
+{dia === fechaHoy() && (
   <div
     className="
       absolute
