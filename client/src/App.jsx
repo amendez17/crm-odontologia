25ae560347c12d9d8f3a73e4e9ec04de57d65230
amendez17 <74200859+amendez17@users.noreply.github.com>
2026-05-15 10:48:51 -0700

    Replace Dashboard route with Citas route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -52,7 +52,7 @@ export default function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<PrivateRoute roles={["administrador", "doctor", "recepcionista"]}><Layout /></PrivateRoute>}>
-          <Route index element={<Dashboard />} />
+          <Route index element={<Citas />} />
         
           <Route path="pacientes" element={ <PrivateRoute roles={["administrador", "doctor", "recepcionista"]}> <Pacientes /> </PrivateRoute> }/>
           <Route path="pacientes/:id" element={ <PrivateRoute roles={["administrador", "doctor", "recepcionista"]}> <PacienteDetalle /> </PrivateRoute>}/>
