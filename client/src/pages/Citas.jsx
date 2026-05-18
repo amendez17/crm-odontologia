1ab423697cf4ce179fd84101455ae8302ea3f4db
amendez17 <74200859+amendez17@users.noreply.github.com>
2026-05-18 12:06:45 -0700

    Change z-index of sticky div in Citas.jsx

--- client/src/pages/Citas.jsx
+++ client/src/pages/Citas.jsx
@@ -668,7 +668,7 @@ const renderVistaSemana = () => {
           return (
             <div
               key={dia}
-              className="border-l p-1 md:p-2 text-center sticky top-0 bg-white z-30"
+              className="border-l p-1 md:p-2 text-center sticky top-0 bg-white z-10"
             >
               <div className="text-[9px] md:text-[9px] md:text-xs text-gray-500">
                 {DIAS_SEMANA[i]}
