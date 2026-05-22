0ec72e4b3f4f57987707a155d46397946c28e139
amendez17 <74200859+amendez17@users.noreply.github.com>
2026-05-21 21:31:18 -0700

    Add middleware to attach io to request object

--- server/src/index.js
+++ server/src/index.js
@@ -30,6 +30,11 @@ app.set('io', io);
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
 // Rutas
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/usuarios', require('./routes/usuarios.routes'));
