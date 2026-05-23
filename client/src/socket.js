85472a064c05475cf783a87deed61fa4d57b8822
amendez17 <74200859+amendez17@users.noreply.github.com>
2026-05-22 19:01:54 -0700

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,10 @@
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_API_URL);
+const socket = io(import.meta.env.VITE_API_URL, {
+  transports: ['websocket'],
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+});
 
 export default socket;
